320182b78c0a974270f56f9fd2babace9f2f0ca4
Robert Bruce <rbruce155@gmail.com>
2018-07-27 08:17:55 -0700

    displaying data from isearch feed

--- src/D8isearchView.js
+++ src/D8isearchView.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Container, Row } from 'reactstrap';
 import './D8isearchView.css';
+import Loader from 'react-loader-spinner';
+import Fade from 'react-reveal/Fade';
 
 class D8isearchPicker extends Component {
 
@@ -58,25 +60,87 @@ class D8isearchPicker extends Component {
   render() {
     let config = this.props.dataFromPage.config
     let results = this.state.ourData.map(( thisNode, index ) => {
-      return(
-        <div key={thisNode.asuriteId}>
-          <p>Name: {thisNode.displayName} ({thisNode.asuriteId})</p>
-        </div>
 
-      )
+      switch (this.props.dataFromPage.theme) {
+        case 'Modern':
+        return(
+            <tr key={thisNode.eid}>
+              <th scope="row">
+                <img className="pictureOriginal" src={thisNode.photoUrl} onError={(e)=>{e.target.src="https://i.pinimg.com/originals/3a/fd/eb/3afdebe186623197d3500c70df74bfc4.png"}} alt={ 'profile picture for ' + thisNode.displayName } />
+              </th>
+              <td>
+                <p>
+                  <a className="linkOriginal" href={ 'https://isearch.asu.edu/profile/' + thisNode.eid }>{thisNode.displayName}</a>
+                </p>
+                <p className="titleOriginal">Modern: {thisNode.primaryTitle}</p>
+                <p>{thisNode.shortBio}</p>
+              </td>
+              <td>
+                <p>
+                  <a className="linkOriginal" href={ 'mailto:' + thisNode.emailAddress }>{thisNode.emailAddress}</a>
+                </p>
+                <p>{thisNode.phone}</p>
+              </td>
+            </tr>
+          )
+          break;
+        default:
+        return(
+            <tr key={thisNode.eid}>
+              <th scope="row">
+                <img className="pictureOriginal" src={thisNode.photoUrl} onError={(e)=>{e.target.src="https://i.pinimg.com/originals/3a/fd/eb/3afdebe186623197d3500c70df74bfc4.png"}} alt={ 'profile picture for ' + thisNode.displayName } />
+              </th>
+              <td>
+                <p>
+                  <a className="linkOriginal" href={ 'https://isearch.asu.edu/profile/' + thisNode.eid }>{thisNode.displayName}</a>
+                </p>
+                <p className="titleOriginal">{thisNode.primaryTitle}</p>
+                <p>{thisNode.shortBio}</p>
+              </td>
+              <td>
+                <p>
+                  <a className="linkOriginal" href={ 'mailto:' + thisNode.emailAddress }>{thisNode.emailAddress}</a>
+                </p>
+                <p>{thisNode.phone}</p>
+              </td>
+            </tr>
+          )
+
+      }
+
     })
 
-    return (
-      <div id="D8isearchPicker">
-        <Container>
-            <Row>
-              <p><strong>Configuration here:</strong> {config}</p>
-              <p><strong>Results here:</strong></p>
-              {results}
-            </Row>
-        </Container>
-      </div>
-    );
+    if ( !this.state.isLoaded ) {
+      return(
+        <div className="loader">
+          <Loader
+           type="ThreeDots"
+           color="#5C6670"
+           height="100"
+           width="100"
+          />
+        </div>
+      )
+    }
+    else {
+      return (
+        <Fade>
+          <div id="D8isearchPicker">
+            <Container>
+                <Row>
+                  <p><strong>Configuration here:</strong> {config}</p>
+                  <table className="table">
+                    <tbody>
+                      {results}
+                    </tbody>
+                  </table>
+                </Row>
+            </Container>
+          </div>
+        </Fade>
+      );
+    }
+
   }
 }
 
