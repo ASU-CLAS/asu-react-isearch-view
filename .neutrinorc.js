cf856b46df8219fab4d84c2f3aa70a18d7faa54e
Nathan Rollins <nathan.rollins@asu.edu>
2021-03-30 23:50:18 -0700

    Update Prettier and ESlint setup

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,17 +1,27 @@
-const airbnb = require("@neutrinojs/airbnb");
-const reactComponents = require("@neutrinojs/react-components");
-const jest = require("@neutrinojs/jest");
+const airbnb = require('@neutrinojs/airbnb');
+const reactComponents = require('@neutrinojs/react-components');
+const jest = require('@neutrinojs/jest');
 
 module.exports = {
   options: {
     root: __dirname,
+    mains: {
+      // Compile isearchPicker
+      D8isearchPicker: 'isearchPicker',
+
+      // Compile profilePage view
+      // profilePage: "profilePage"
+    },
   },
   use: [
-    airbnb(),
+    process.env.NODE_ENV === 'development' ? airbnb() : false,
     reactComponents(),
     jest(),
-    (neutrino) => {
-      neutrino.config.externals({ react: "React", "react-dom": "ReactDOM" });
+    neutrino => {
+      neutrino.config.externals({
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      });
     },
   ],
 };
