5eaad636cb84240ddb25e96c83cf266b749cd047
John Cox <john.k.cox@asu.edu>
2022-06-27 14:49:17 -0700

    merge conflict

--- src/containers/IsearchCircleList.js
+++ src/containers/IsearchCircleList.js
@@ -1,8 +1,6 @@
-// ./src/components/App.js
-import React from 'react'
-import {IsearchCircleView} from '../components/IsearchCircleView'
-import Loader from 'react-loader-spinner'
-import Avatar from "../components/images/avatar.png"
+import React from 'react';
+import {IsearchCircleView} from '../components/IsearchCircleView';
+import Loader from 'react-loader-spinner';
 
 export const IsearchCircleList = ({
   profileList,
@@ -12,11 +10,6 @@ export const IsearchCircleList = ({
   return (
     <div className="isearch-circle-list">
     <div className="container"><div className="row">
-<<<<<<< HEAD
-      {profileList.map((profileData, index) => (
-        profileData.listConfig = listConfig,
-        <div className="col col-6 modernCol col-md-4 col-lg-3"><IsearchCircleView key={index} {...profileData} /></div>
-=======
       {console.log(`circle list data:`)}
       {console.log(profileList)}
       {profileList.map((profile, index) => (
@@ -24,7 +17,6 @@ export const IsearchCircleList = ({
         profile.listConfig = listConfig,
         <div className="col col-6 modernCol col-md-4 col-lg-3" key={index}><IsearchCircleView {...profile} /></div>
         
->>>>>>> a5e531f13c3237f4487295c94add3ecb33a52596
       ))}
     </div></div></div>
   );
@@ -32,7 +24,7 @@ export const IsearchCircleList = ({
 
 IsearchCircleList.defaultProps = {
   listConfig: {
-    defaultPhoto: Avatar,
+    defaultPhoto: 'https://thecollege.asu.edu/profiles/openclas/modules/custom/clas_isearch/images/avatar.png',
     showBio: true,
     showEmail: true,
     showPhone: true,
