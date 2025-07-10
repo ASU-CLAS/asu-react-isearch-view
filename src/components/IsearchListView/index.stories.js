e3d71fed90ac0405cc86e2f17cb3d42a1c21a114
especiale <especial@asu.edu>
2025-07-10 11:41:50 -0700

    remove references to local stylesheet

--- src/components/IsearchListView/index.stories.js
+++ src/components/IsearchListView/index.stories.js
@@ -2,7 +2,6 @@ import faker from 'faker';
 
 import {IsearchListView} from '.';
 
-// import '../../../public/bootstrap-asu.css';
 import '@asu/unity-bootstrap-theme/dist/css/unity-bootstrap-theme.css'
 
 export default {
