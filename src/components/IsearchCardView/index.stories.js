b79036c437f9e78aeedf3d04a0656749b349d307
especiale <especial@asu.edu>
2025-07-10 11:39:53 -0700

    update component to consume @asu unity packages

--- src/components/IsearchCardView/index.stories.js
+++ src/components/IsearchCardView/index.stories.js
@@ -2,7 +2,9 @@ import faker from 'faker';
 
 import {IsearchCardView} from '.';
 
-import '../../../public/bootstrap-asu.css';
+// import '../../../public/bootstrap-asu.css';
+
+import '@asu/unity-bootstrap-theme/dist/css/unity-bootstrap-theme.css';
 
 export default {
   title: 'iSearch/CardView',
